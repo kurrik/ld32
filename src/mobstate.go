6aa725d45f31f589ea7c42154f9d80d6dbfc20ef
Wes <readybreak@gmail.com>
2015-04-18 15:15:34 -0700

    Refactor mobstate to use a few more interfaces. Begin implementing the world's dumbest boss.

--- src/mobstate.go
+++ src/mobstate.go
@@ -14,13 +14,19 @@
 
 package main
 
-type Mob struct {
-	detectionRadius float32
+import "time"
+
+type Mob interface {
+	Bored(time.Duration) bool
 }
 
-func (m *Mob) Bored(t float32) bool {
-	// TODO; Mobs never get bored. Probably they should.
-	return false
+type Mobile struct {
+	DetectionRadius float32
+	BoredThreshold  time.Duration
+}
+
+func (m *Mobile) Bored(d time.Duration) bool {
+	return d >= m.BoredThreshold
 }
 
 // MobState is implemented by various states responsible for controlling mobile
@@ -32,14 +38,14 @@ type MobState interface {
 	// which state the mobile should transition to. It's legal to return
 	// either the current MobState or nil, indicating that the mobile
 	// should transition to a previous state.
-	ExamineWorld(*Mob, *GameLayer) (newState MobState)
+	ExamineWorld(Mob, *Level) (newState MobState)
 	// Update should be called each frame and may update values in the
 	// current state or call functions on the mob.
-	Update(*Mob)
+	Update(Mob, time.Duration)
 	// Enter should be called when entering this MobState.
-	Enter(*Mob)
+	Enter(Mob)
 	// Enter should be called when exiting this MobState.
-	Exit(*Mob)
+	Exit(Mob)
 }
 
 // SearchState is the state during which a mobile is aimlessly wandering,
@@ -48,33 +54,37 @@ type SearchState struct{}
 
 // ExamineWorld returns HuntState if the player is seen, otherwise the mob
 // continues wandering.
-func (s *SearchState) ExamineWorld(m *Mob, l *GameLayer) MobState {
+func (s *SearchState) ExamineWorld(m Mob, l *Level) MobState {
 	if playerSeen(m, l) {
 		return &HuntState{}
 	}
 	return s
 }
 
-func (s *SearchState) Update(m *Mob) {
+func (s *SearchState) Update(m Mob, d time.Duration) {
 }
 
-func (s *SearchState) Enter(m *Mob) {
+func (s *SearchState) Enter(m Mob) {
 	// TODO: set some hunting animation.
 }
 
-func (s *SearchState) Exit(m *Mob) {
+func (s *SearchState) Exit(m Mob) {
 	// TODO: maybe something should happen when we start hunting?
 }
 
 // HuntState is the state during which a mobile is actively hunting the player.
 type HuntState struct {
-	timeSinceLastContact float32
+	durSinceLastContact time.Duration
 }
 
 // ExamineWorld returns the current state if the player is currently seen or
 // the mob is not yet tired of chasing. Otherwise, it returns nil.
-func (h *HuntState) ExamineWorld(m *Mob, l *GameLayer) MobState {
-	if playerSeen(m, l) || !m.Bored(h.timeSinceLastContact) {
+func (h *HuntState) ExamineWorld(m Mob, l *Level) MobState {
+	if playerSeen(m, l) {
+		h.durSinceLastContact = time.Duration(0)
+		return h
+	}
+	if !m.Bored(h.durSinceLastContact) {
 		return h
 	}
 	return nil
@@ -82,24 +92,19 @@ func (h *HuntState) ExamineWorld(m *Mob, l *GameLayer) MobState {
 
 // Update resets the player's hiding timer if the player is seen, otherwise it
 // increments.
-func (h *HuntState) Update(m *Mob) {
-	// Maybe this should be in ExamineWorld???
-	//	if playerSeen(m, l) {
-	//		h.timeSinceLastContact = 0
-	//		return
-	//	}
-	h.timeSinceLastContact++
+func (h *HuntState) Update(m Mob, d time.Duration) {
+	h.durSinceLastContact += d
 }
 
-func (h *HuntState) Enter(m *Mob) {
+func (h *HuntState) Enter(m Mob) {
 }
 
-func (h *HuntState) Exit(m *Mob) {
+func (h *HuntState) Exit(m Mob) {
 }
 
 // playerSeen returns true if the player is currently visible to the mob and
 // within its detection radius.
-func playerSeen(m *Mob, l *GameLayer) bool {
+func playerSeen(m Mob, l *Level) bool {
 	// TODO: do something with visibility and detection radius.
 	return true
 }
